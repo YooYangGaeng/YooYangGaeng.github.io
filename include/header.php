6852401f1736840fe6bbb1e9c66807689df55c99
Reen <0726ouleesun@gmail.com>
2025-05-23 15:55:56 +0900

    Thinking about design..

--- include/header.php
+++ include/header.php
@@ -1,22 +1,12 @@
 <header id="header">
     <h1 class="logo">
-        <a href="/">Yang Gaeng</a>
+        <a href="/">Yang Gaeng®</a>
     </h1>
+
     <div class="gnb">
-        
+        <ul>
+            <li><a class="active" href="javascript:void(0)">profile</a></li>
+            <li><a href="javascript:void(0)">works</a></li>
+        </ul>
     </div>
 </header>
-<script>
-    window.addEventListener("load", function(){
-     
-        // 모바일 메뉴
-        $("#header .gnb > ul > li > a").on("click",function(){
-            if(!$(this).parent().hasClass("depth01")){
-                // 2depth가 있을 경우
-                $(this).parent("li").toggleClass("on").siblings().removeClass("on");
-                $(this).next(".depth02").stop(true,true).slideToggle().parent("li").siblings().find(".depth02").slideUp();
-                //return false;
-            }
-        });
-    })
-</script>
--- index.php
+++ index.php
@@ -4,6 +4,22 @@
     <?php include './include/headInclude.php';?>
 </head>
 <body>
-    <?php include './include/header.php';?>
+    <section class="document_section">
+        <div class="document_head">
+            <?php include './include/header.php';?>
+        </div>
+        <div class="document_content">
+            <div class="info_wrap">
+                <div class="info_heading">
+                    <div class="img"></div>
+                    <ul class="info_list">
+                        <li>Ytestn</li>
+                        <li>@naver.co</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
 </body>
 </html>
